a3f193f4fffd7ce043868d4c6b1399124efc65da
KINGROKOKO <themerc01@gmail.com>
2017-08-14 21:56:18 +0100

    input

--- directory.rb
+++ directory.rb
@@ -13,6 +13,24 @@ students = [
 {name:"Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, chort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+ # return the array of students
+    students
+  end
+
 def print_header
 puts "The students of Villains Academy"
 puts "____________"
@@ -23,10 +41,10 @@ students.each{|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
 
-#nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
