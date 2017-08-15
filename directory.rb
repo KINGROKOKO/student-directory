c7f23710cc9eeef44690de53e1965309fd21e96c
KINGROKOKO <themerc01@gmail.com>
2017-08-16 00:40:19 +0100

    while loop, specific character & under 12 characters

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
+
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.upcase
+  puts "thanks now nationality?"
+  nationality = gets.chomp.upcase
+  puts "thanks now favourite food?"
+  food = gets.chomp.upcase
+  puts "and favourite number"
+  number = gets.chomp.to_s.upcase
+
   # while the name is not empty, repeat this code
   while !name.empty? do
-    students << {name: name, chort: :november}
+    students << {name: name, nationality: nationality, food: food, number: number, cohort: :november}
     puts "Now we have #{students.count} students"
+
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.upcase
+    puts "thanks now nationality?"
+    nationality = gets.chomp.upcase
+     puts "thanks now favourite food?"
+    food = gets.chomp.upcase
+     puts "and favourite number"
+    number = gets.chomp.to_s.upcase
   end
+
  # return the array of students
     students
   end
@@ -22,7 +39,16 @@ puts "____________"
 end
 
 def print(students)
-students.each_with_index{|student, index| puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("N","n")&& student[:name].size<12}
+  i=0
+
+  while i < students.length do
+
+    puts "#{students[i][:name]} from #{students[i][:nationality]} favourite food #{students[i][:food]} favourite number #{students[i][:number]} (#{students[i][:cohort]} cohort)" if students[i][:name].start_with?("N","n") && students[i][:name].size<12
+
+    i += 1
+
+   end
+
 end
 
 def print_footer(names)
