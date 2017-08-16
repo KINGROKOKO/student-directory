536a5a481b9d77199206ddffc252fd69cbae28eb
KINGROKOKO <themerc01@gmail.com>
2017-08-16 02:20:59 +0100

    center, ask cohort/name, check empty value, convert sym

--- directory.rb
+++ directory.rb
@@ -6,27 +6,41 @@ def input_students
   students = []
 
   # get the first name
+
   name = gets.chomp.upcase
   puts "thanks now nationality?"
+
   nationality = gets.chomp.upcase
   puts "thanks now favourite food?"
+
   food = gets.chomp.upcase
   puts "and favourite number"
+
   number = gets.chomp.to_s.upcase
+  puts "and cohort?"
+
+  cohort = gets.chomp.upcase.to_sym
+
+    cohort.empty?
+    puts "you are being assigned a default cohort"
+    cohort = "Cohort Default".to_sym
 
   # while the name is not empty, repeat this code
   while !name.empty? do
-    students << {name: name, nationality: nationality, food: food, number: number, cohort: :november}
+    students << {name: name, nationality: nationality, food: food, number: number, cohort: cohort}
     puts "Now we have #{students.count} students"
 
     # get another name from the user
     name = gets.chomp.upcase
+
     puts "thanks now nationality?"
     nationality = gets.chomp.upcase
      puts "thanks now favourite food?"
     food = gets.chomp.upcase
      puts "and favourite number"
     number = gets.chomp.to_s.upcase
+    puts "and cohort?"
+  cohort = gets.chomp.upcase.to_sym
   end
 
  # return the array of students
@@ -43,7 +57,7 @@ def print(students)
 
   while i < students.length do
 
-    puts "#{students[i][:name]} from #{students[i][:nationality]} favourite food #{students[i][:food]} favourite number #{students[i][:number]} (#{students[i][:cohort]} cohort)" if students[i][:name].start_with?("N","n") && students[i][:name].size<12
+    puts "#{students[i][:name]} from #{students[i][:nationality]} enjoys #{students[i][:food]} fave number #{students[i][:number]} (#{students[i][:cohort]} cohort)".center(50) if students[i][:name].start_with?("N","n") && students[i][:name].size<12
 
     i += 1
 
@@ -52,7 +66,7 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50, '---')
 end
 
 students = input_students
